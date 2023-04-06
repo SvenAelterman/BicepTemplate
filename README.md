d046bc1e6a93318fb0b6980b930dcf6cc9f595d2
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2023-04-06 15:29:50 -0500

    Support for parameter file Closes #7

--- README.md
+++ README.md
@@ -6,11 +6,17 @@ Template repo for creating a new Bicep deployment.
 
 ### main.bicep
 
-This is the template that will be deployed. You should add resources or reference modules here.
+This is the template that will be deployed. You should add modules here.
 
 ### deploy.ps1
 
 This PowerShell script will deploy your main.bicep template.
+
+`./deploy.ps1 -UseParameterFile`
+
+-- OR --
+
+`./deploy.ps1 -UsePSParameters -Environment 'test' ...`
 
 ### common-modules
 
@@ -24,16 +30,16 @@ You should create your own modules folder and reference those modules from main.
 
 Here are the common parameters defined by the template main.bicep:
 
-* **location**: The Azure region to target for deployments.
-* **environment**: An environment value, such as "dev."
-* **workloadName**: The name of the workload to be deployed. This will be used to name deployments and to complete the naming convention.
-* **sequence** (optional, defaults to `1`)
-* **tags** (optional, defaults to none)
-* **namingConvention** (optional, defaults to `{rtype}-{wloadname}-{env}-{loc}-{seq}`): the structure of the Azure resources names. Use placeholders as follows:
-  * **{rtype}**: The resource type. Your main.bicep should replace {rtype} with the recommended Azure resource type abbreviation as found at <https://docs.microsoft.com/azure/cloud-adoption-framework/ready/azure-best-practices/resource-abbreviations>.
-  * **{wloadname}**: Replaced with the value of the `workloadName` parameter.
-  * **{env}**: Replaced with the value of the `environment` parameter.
-  * **{loc}**: Replaced with the value of the `location` parameter.
-  * **{seq}**: Replaced with the string value of the sequence parameter, always formatted as two digits.
+- **location**: The Azure region to target for deployments.
+- **environment**: An environment value, such as "dev."
+- **workloadName**: The name of the workload to be deployed. This will be used to name deployments and to complete the naming convention.
+- **sequence** (optional, defaults to `1`)
+- **tags** (optional, defaults to none)
+- **namingConvention** (optional, defaults to `{rtype}-{workloadName}-{env}-{loc}-{seq}`): the structure of the Azure resources names. Use placeholders as follows:
+  - **{rtype}**: The resource type. Your main.bicep should replace {rtype} with the recommended Azure resource type abbreviation as found at <https://docs.microsoft.com/azure/cloud-adoption-framework/ready/azure-best-practices/resource-abbreviations>.
+  - **{workloadName}**: Replaced with the value of the `workloadName` parameter.
+  - **{env}**: Replaced with the value of the `environment` parameter.
+  - **{loc}**: Replaced with the value of the `location` parameter.
+  - **{seq}**: Replaced with the string value of the sequence parameter, always formatted as two digits.
 
-These parameters are passed to the deployment from the PowerShell script using the `$TemplateParameters` PowerShell object, which uses parameter splatting for increased resilience.
+When not using a template parameter file, these parameters are passed to the deployment from the PowerShell script using a `$TemplateParameters` PowerShell object, which uses parameter splatting for increased resilience.
--- deploy.ps1
+++ deploy.ps1
@@ -7,38 +7,75 @@
 [CmdletBinding()]
 Param(
 	[ValidateSet('eastus2', 'eastus')]
+	[Parameter()]
 	[string]$Location = 'eastus',
-	# The environment descriptor
+	# Using PS function parameters
+	[Parameter(ParameterSetName = 'NoParamFile')]
+	[switch]$UsePSParameters,
 	[ValidateSet('test', 'demo', 'prod')]
-	[string]$Environment = 'test',
-	#
-	[Parameter(Mandatory = $true)]
-	[string]$WorkloadName,
-	#
-	[int]$Sequence = 1,
-	[string]$NamingConvention = "{rtype}-{wloadname}-{env}-{loc}-{seq}"
+	[Parameter(ParameterSetName = 'NoParamFile', Mandatory)]
+	[string]$Environment,
+	# The Mandatory keyword here allows differentiation between the parameter sets
+	[Parameter(ParameterSetName = 'NoParamFile', Mandatory)]
+	[string]$WorkloadName = 'biceptemplate2',
+	[Parameter(ParameterSetName = 'NoParamFile', Mandatory)]
+	[int]$Sequence,
+	[Parameter(ParameterSetName = 'NoParamFile')]
+	[string]$NamingConvention = "{rtype}-{workloadName}-{env}-{loc}-{seq}",
+	# Using parameters file
+	[Parameter(ParameterSetName = 'ParamFile')]
+	[switch]$UseParameterFile,
+	[Parameter(ParameterSetName = 'ParamFile', Position = 1)]
+	[string]$TemplateParameterFile = "./main.parameters-sample.jsonc"
 )
 
-$TemplateParameters = @{
-	# REQUIRED
-	location         = $Location
-	environment      = $Environment
-	workloadName     = $WorkloadName
-
-	# OPTIONAL
-	sequence         = $Sequence
-	namingConvention = $NamingConvention
-	tags             = @{
-		'date-created' = (Get-Date -Format 'yyyy-MM-dd')
-		purpose        = $Environment
-		lifetime       = 'short'
-	}
+# Define common parameters for the New-AzDeployment cmdlet
+[hashtable]$CmdLetParameters = @{
+	Location     = $Location
+	TemplateFile = '.\main.bicep'
 }
 
-$DeploymentResult = New-AzDeployment -Location $Location -Name "$WorkloadName-$Environment-$(Get-Date -Format 'yyyyMMddThhmmssZ' -AsUTC)" `
-	-TemplateFile ".\main.bicep" -TemplateParameterObject $TemplateParameters
+if ($UseParameterFile) {
+	Write-Verbose "Using template parameter file"
+	$CmdLetParameters.Add('TemplateParameterFile', $TemplateParameterFile)
+	# Read the values from the parameters file, to use when generating the $DeploymentName value
+	$ParameterFileContents = (Get-Content $TemplateParameterFile | ConvertFrom-Json).parameters
+	$WorkloadName = $ParameterFileContents.workloadName.value
+	$Environment = $ParameterFileContents.environment.value
+}
+else {
+	Write-Verbose "Creating template parameter object"
+	# Create a template parameter object based on the PS parameters
+	$TemplateParameters = @{
+		# REQUIRED
+		location         = $Location
+		environment      = $Environment
+		workloadName     = $WorkloadName
+	
+		# OPTIONAL
+		sequence         = $Sequence
+		namingConvention = $NamingConvention
+		tags             = @{
+			'date-created' = (Get-Date -Format 'yyyy-MM-dd')
+			purpose        = $Environment
+			lifetime       = 'short'
+		}
+	}
 
-$DeploymentResult
+	$CmdLetParameters.Add('TemplateParameterObject', $TemplateParameters)
+}
 
+# Generate a unique name for the deployment
+[string]$DeploymentName = "$WorkloadName-$Environment-$(Get-Date -Format 'yyyyMMddThhmmssZ' -AsUTC)"
+$CmdLetParameters.Add('Name', $DeploymentName)
+
+# Execute the deployment
+$DeploymentResult = New-AzDeployment @CmdLetParameters
+
+# Evaluate the deployment results
 if ($DeploymentResult.ProvisioningState -eq 'Succeeded') {
+	Write-Host "🔥 Deployment succeeded."
+}
+else {
+	$DeploymentResult
 }
